1afd6de249b735f0d9e192466d3077ae02d3c63e
EstebanDev <esteban.villasecatt@gmail.com>
2024-08-21 00:25:24 -0400

    add .gitignore

--- main/settings.py
+++ main/settings.py
@@ -1,5 +1,6 @@
-import os
+from decouple import config, Csv
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,12 +10,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(ftx^bw8l^&(&=rstlrk+c62f#%befwko&tndmp=n^fieo3b3v'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
